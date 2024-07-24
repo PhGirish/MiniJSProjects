99cda7c9f772810c66f4885b64971ccbf0302efa
PhGirish <girizph8@gmail.com>
2024-07-24 22:29:33 +0530

    half way

---
+++ stopwatch/index.js
@@ -0,0 +1,4 @@
+const hour = document.getElementById("hour");
+const min = document.getElementById("min");
+const sec = document.getElementById("sec");
+const startbtn = document.getElementById("start");
